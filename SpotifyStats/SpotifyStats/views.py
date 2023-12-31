7c82b942aa1d527eb1063658b30b10b4ca26b5f3
Justin Ng <justinchunng@gmail.com>
2023-12-31 15:31:39 -0500

    Added login html page and files attached to it

--- SpotifyStats/SpotifyStats/views.py
+++ SpotifyStats/SpotifyStats/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+def signin(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'login.html')
