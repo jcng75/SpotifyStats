dfdcd2e5c9d83ef2e62a454ee6f8d1ac8f920c27
Justin Ng <justinchunng@gmail.com>
2024-01-03 15:51:47 -0500

    Added about page

--- SpotifyStats/SpotifyStats/urls.py
+++ SpotifyStats/SpotifyStats/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.signin, name='login'),
     path('register/', views.register, name='register'),
+    path('about/', views.about, name='about'),
     path('testNav/', views.testNav, name='testNav')
 ]
--- SpotifyStats/SpotifyStats/views.py
+++ SpotifyStats/SpotifyStats/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'landingPage.html')
 
 def signin(request):
     return render(request, 'login.html')
 
 def register(request):
     return render(request, 'register.html')
+
+def about(request):
+    return render(request, 'about.html')
 
 def testNav(request):
     return render(request, 'navbar1.html')
